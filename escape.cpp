52235b2c23fcabfdc5f00ea7d1bc2424838930bd
s1140449-debug <s1140449@dmhs.kh.edu.tw>
2026-05-22 10:54:39 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1.快樂葳孟" << endl;
+	cout << "2.快樂 bobo" << endl;
+	cout << "3.快樂\t丞盈" << endl;
+	cout << "4.快樂\n靖涵" << endl;
+	cout << "\"5.快樂kiki\"" << endl;
+	cout << "\'6.快樂葳孟\'" << endl;
+	cout << "\\7.快樂葳孟\\" << endl;
+	return 0; 
+}
